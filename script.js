ea3396103408cb19ee3adb592da9509121aaddf5
JeremyKomarov <komarovj91@gmail.com>
2025-07-01 19:54:11 +0300

    Added Js Basic Structure

---
+++ script.js
@@ -0,0 +1,6 @@
+const clickMeEl = document.querySelector("clickMe");
+const messageEl = document.querySelector(".message");
+
+clickMeEl.addEventListener("click", () => {
+  messageEl.textContent = "You clicked the button! Well done!";
+});
